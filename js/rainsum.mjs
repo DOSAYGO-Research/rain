b05c67a6444aeff4939006fe16aed51e3e5ff490
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-07-17 12:07:29 +0800

    JS version works well and is consistent for text files but produces different results for binary files. WASM version probably needs to handle BUffer

--- js/rainsum.mjs
+++ js/rainsum.mjs
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import fs from 'fs';
 import process from 'process';
 import yargs from 'yargs';
@@ -5,6 +6,7 @@ import { hideBin } from 'yargs/helpers';
 import { rainstormHash } from './lib/rainstorm.mjs';
 
 const argv = yargs(hideBin(process.argv))
+    .usage('Usage: $0 [options] [file]')
     .option('mode', {
         alias: 'm',
         description: 'Mode: digest or stream',
@@ -25,42 +27,45 @@ const argv = yargs(hideBin(process.argv))
         description: 'Output file for the hash',
         type: 'string',
     })
-    .option('input-file', {
-        alias: 'i',
-        description: 'Input file to hash',
+    .option('seed', {
+        description: 'Seed value',
         type: 'string',
     })
     .help()
     .alias('help', 'h')
+    .demandCommand(0, 1) // Accepts 0 or 1 positional arguments
     .argv;
 
-async function hashBuffer(mode, algorithm, seed, buffer, outputStream, hashSize) {
-    const hash = await rainstormHash(hashSize, seed, buffer.toString());
-    outputStream.write(hash);
+async function hashBuffer(mode, algorithm, seed, buffer, outputStream, hashSize, inputName) {
+    const hash = await rainstormHash(hashSize, seed, buffer.toString('binary'));
+    outputStream.write(`${hash} ${inputName}\n`);
 }
 
 async function hashAnything(mode, algorithm, seed, inputPath, outputPath, size) {
     let buffer = [];
+    let inputName;
     if (inputPath === '/dev/stdin') {
         for await (const chunk of process.stdin) {
             buffer.push(chunk);
         }
         buffer = Buffer.concat(buffer);
+        inputName = 'stdin';
     } else {
         buffer = fs.readFileSync(inputPath);
+        inputName = inputPath;
     }
 
-    const outputStream = fs.createWriteStream(outputPath);
-    await hashBuffer(mode, algorithm, seed, buffer, outputStream, size);
+    const outputStream = fs.createWriteStream(outputPath, { flags: 'a' });
+    await hashBuffer(mode, algorithm, seed, buffer, outputStream, size, inputName);
 }
 
 async function main() {
     const mode = argv.mode || 'digest';
     const algorithm = argv.algorithm || 'rainstorm';
     const size = argv.size || 256;
-    const inputPath = argv['input-file'] || '/dev/stdin';
-    const outputPath = argv['output-file'] || '/dev/stdout';
     const seed = BigInt(argv['seed'] || 0);
+    const inputPath = argv._[0] || argv['input-file'] || '/dev/stdin';
+    const outputPath = argv['output-file'] || '/dev/stdout';
 
     await hashAnything(mode, algorithm, seed, inputPath, outputPath, size);
 }
