c1a37d5b0dc278ba3312f9755d333685830a6359
DOSAYGO Development Team <development.team@dosyago.com>
2024-12-15 11:07:09 +0800

    ok

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,8 @@
 if [ ! -f src/cxxopts.hpp ]; then
   curl -L https://raw.githubusercontent.com/jarro2783/cxxopts/eb787304d67ec22f7c3a184ee8b4c481d04357fd/include/cxxopts.hpp -o src/cxxopts.hpp
 fi
+
+export PATH="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:${PATH}"
+
 make
 
